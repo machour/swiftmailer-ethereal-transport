0644c09d8eddd72ab8ed49b8ceba125efdff03b7
Mehdi Achour <machour@gmail.com>
2022-09-17 16:28:03 +0100

    Initial checkin

---
+++ src/EtherealTransport.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Machour\SwiftMailerEtherealTransport;
+
+use Closure;
+
+/**
+ * @property array[] $plugins
+ */
+class EtherealTransport extends \Swift_SmtpTransport
+{
+    public const EVENT_TRANSPORT_DONE = 'transportDone';
+
+    public ?Closure $callback = null;
+
+    public function __construct($host = 'smtp.ethereal.email', $port = 587, $encryption = 'tls')
+    {
+        parent::__construct($host, $port, $encryption);
+    }
+
+    public function executeCommand($command, $codes = [], &$failures = null, $pipeline = false, $address = null): ?string
+    {
+        $response = parent::executeCommand($command, $codes, $failures, $pipeline, $address);
+
+        if ($this->callback && preg_match('!^250 Accepted \[STATUS=new MSGID=(.*)]$!', trim($response), $matches)) {
+            call_user_func($this->callback, "https://ethereal.email/message/$matches[1]");
+        }
+
+        return $response;
+    }
+}
